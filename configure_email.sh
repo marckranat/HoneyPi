15468f65a5ebd275e39765ceec53bbc71f55452b
Bogdan Cristea <cristeab@gmail.com>
2018-02-01 12:57:54 +0200

    add script for configuring e-mail client

---
+++ configure_email.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+#check root
+if [ $UID -ne 0 ]; then
+	echo "Please run this script as root: sudo $0"
+	exit 1
+fi
+
+echo "Install required dependences..."
+apt install -y ssmtp
+
+#outgoing e-mail server address
+echo -n "Provide outgoing e-mail server address [smtp.gmail.com]:"
+read mailhub
+
+#outgoing server port
+echo -n "Provide outgoing e-mail server port [587]:"
+read port
+
+#enable STARTTLS
+echo -n "Enable STARTTLS (y/n) [Y]?:"
+read starttls_yesno
+
+#username
+echo -n "Provide username of your e-mail account:"
+read username
+
+#passsword
+echo -n "Provide password for your e-mail account:"
+read password
+
+#setup SSMTP configuration
+function setup_ssmtp_conf() {
+	SED_CMD="\@$1=@d"
+	sed -i -e "$SED_CMD" /etc/ssmtp/ssmtp.conf
+	SED_CMD="\$a$1=$2"
+	sed -i -e "$SED_CMD" /etc/ssmtp/ssmtp.conf
+}
+setup_ssmtp_conf "mailhub" "${mailhub}:{$port}"
+if [ "Y" == "$starttls_yesno" ] || [ "y" == "$starttls_yesno" ]; then
+	setup_ssmtp_conf "UseSTARTTLS" "YES"
+else
+	setup_ssmtp_conf "UseSTARTTLS" "NO"
+fi
+setup_ssmtp_conf "AuthUser" "${username}"
+setup_ssmtp_conf "AuthPass" "${password}"
+
+#test configuration
+echo -n "Do you want to test the configured e-mail client (y/n) [Y]?:"
+read test_yesno
+if [ "Y" == "$test_yesno" ] || [ "y" == "$test_yesno" ]; then
+	echo -n "Provide an e-mail address where the message will be send: "
+	read email
+	echo "Sending a test message to $email..."
+	echo "test from RPi" | ssmtp $email
+	echo "Done"
+fi
