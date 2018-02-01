6f81d619440f042ba235af6dd772c8800136a835
Bogdan Cristea <cristeab@gmail.com>
2018-02-01 13:07:04 +0200

    corr

--- configure_email.sh
+++ configure_email.sh
@@ -12,14 +12,23 @@ apt install -y ssmtp
 #outgoing e-mail server address
 echo -n "Provide outgoing e-mail server address [smtp.gmail.com]: "
 read mailhub
+if [ -z "$mailhub" ]; then
+	mailhub="smtp.gmail.com"
+fi
 
 #outgoing server port
 echo -n "Provide outgoing e-mail server port [587]: "
 read port
+if [ -z "$port" ]; then
+	port="587"
+fi
 
 #enable STARTTLS
 echo -n "Enable STARTTLS (y/n) [Y]?: "
 read starttls_yesno
+if [ -z "$starttls_yesno" ]; then
+	starttls_yesno="Y"
+fi
 
 #username
 echo -n "Provide username of your e-mail account: "
@@ -48,6 +57,9 @@ setup_ssmtp_conf "AuthPass" "$password"
 #test configuration
 echo -n "Do you want to test the configured e-mail client (y/n) [Y]?:"
 read test_yesno
+if [ -z "$test_yesno" ]; then
+	test_yesno="Y"
+fi
 if [ "Y" == "$test_yesno" ] || [ "y" == "$test_yesno" ]; then
 	echo -n "Provide an e-mail address where the message will be send: "
 	read email
